9e10dfedd5cc6511a2765093d7824a4638c45dd8
Mukesh Reddy <mukeshvancha122@gmail.com>
2025-07-18 15:08:18 -0400

    product config files

---
+++ common-security/src/test/java/com/amazon_backend/common_security/CommonSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon_backend.common_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
